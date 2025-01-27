2b8487c73a6540944188650d3df363514da3281a
Nicolas Carreras <carreras.contact@gmail.com>
2025-01-27 14:15:49 +0100

    feat: add id management to InputFieldInline component

--- src/components/ui/InputFieldInline/InputFieldInline.tsx
+++ src/components/ui/InputFieldInline/InputFieldInline.tsx
@@ -1,4 +1,4 @@
-import { memo } from "react";
+import { memo, useId } from "react";
 import { Input, InputProps } from "../Input/Input";
 
 type InputFieldInlineProps = InputProps & {
@@ -13,10 +13,13 @@ export const InputFieldInline = memo(function InputFieldInline({
   size = "md",
   onChange,
 }: InputFieldInlineProps) {
+  const id = useId();
   return (
     <div className="flex flex-row gap-2 items-center">
-      <label className="text-zinc-600 font-medium">{label}</label>
-      <Input value={value} placeholder={placeholder} variant={variant} size={size} onChange={onChange} />
+      <label htmlFor={id} className="text-zinc-600 font-medium">
+        {label}
+      </label>
+      <Input id={id} value={value} placeholder={placeholder} variant={variant} size={size} onChange={onChange} />
     </div>
   );
 });
