c3c3855560091a387b6892da6d9f6bd9166487b6
Nicolas Carreras <carreras.contact@gmail.com>
2025-01-28 17:04:51 +0100

    feat: align input labels to the bottom so they're all aligned

--- src/components/ui/InputField/Label.tsx
+++ src/components/ui/InputField/Label.tsx
@@ -1,5 +1,5 @@
 import { memo, PropsWithChildren } from "react";
 
 export const Label = memo(function Label({ children }: PropsWithChildren) {
-  return <div className="flex flex-row justify-between items-center">{children}</div>;
+  return <div className="flex flex-row justify-between items-end">{children}</div>;
 });
